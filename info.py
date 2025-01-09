e743955fdbdeabe26078ae3e184f04ad700a1628
TryToLiveAlon_owner <156423343+TryToLiveAlon@users.noreply.github.com>
2025-01-09 14:04:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -73,7 +73,7 @@ START_IMG = (environ.get('START_IMG', 'https://envs.sh/9_U.mp4 https://envs.sh/9
 FORCESUB_IMG = environ.get('FORCESUB_IMG', 'https://i.ibb.co/ZNC1Hnb/ad3f2c88a8f2.jpg')
 REFER_PICS = (environ.get("REFER_PICS", "https://envs.sh/PSI.jpg")).split() 
 PAYPICS = (environ.get('PAYPICS', 'https://envs.sh/r4H.jpg')).split()
-SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://graph.org/file/9f3f47c690bbcc67633c2.jpg'))
+SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://envs.sh/9Z2.jpg'))
 REACTIONS = ["👀", "😱", "🔥", "😍", "🎉", "🥰", "😇", "⚡"]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
