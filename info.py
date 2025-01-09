fccc6734e6e334ed1fd9b365dfe2ae0712fddb53
TryToLiveAlon_owner <156423343+TryToLiveAlon@users.noreply.github.com>
2025-01-09 10:49:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,11 +18,11 @@ API_HASH = environ.get('API_HASH', '700092e37d7da9a7b781994b7503a488')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
-USERNAME = environ.get('USERNAME', "https://t.me/") # ADMIN USERNAME
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6660736046').split()]
+USERNAME = environ.get('USERNAME', "https://t.me/DE4T8") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/DeathMovies_request')
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002086509550').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 DATABASE_URI = environ.get('DATABASE_URI', "postgresql://fallenrobot_user:f6BxpTPxq5nKAtXjtUOmbfEoVtBGOWS7@dpg-ctrnp4bqf0us73dhnri0-a/fallenrobot")
