e2e557ddfcb1ec9bdef1cc1ec19fe13ba36d52be
SPIDEY OFFICIAL <gaminghatyar777@gmail.com>
2025-04-10 22:01:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '')
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6243694287 6660736046').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/DeathSupportBot") # ADMIN USERNAME
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002097438993'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002097438993 -1002423451263'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/DeathMovies_request')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002086509550').split()]
 #---------------------------------------------------------------
