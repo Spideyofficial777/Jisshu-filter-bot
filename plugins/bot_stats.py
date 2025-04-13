a604dfdde0dae746db65fc2e6febb4576a683c5e
SPIDEY OFFICIAL <gaminghatyar777@gmail.com>
2025-04-14 01:13:35 +0530

    Update bot_stats.py

--- plugins/bot_stats.py
+++ plugins/bot_stats.py
@@ -96,3 +96,20 @@ async def get_ststs(bot, message):
     ram = psutil.virtual_memory().percent
     cpu = psutil.cpu_percent()
     await message.reply_text(script.STATUS_TXT.format(users, groups, size, free, files, db2_size, db2_free, uptime, ram, cpu))
+
+@Client.on_message(filters.command('users') & filters.user(ADMINS))
+async def list_users(bot, message):
+    raju = await message.reply('Getting List Of Users')
+    users = await db.get_all_users()
+    out = "Users Saved In DB Are:\n\n"
+    async for user in users:
+        out += f"<a href=tg://user?id={user['id']}>{user['name']}</a>"
+        if user['ban_status']['is_banned']:
+            out += '( Banned User )'
+        out += '\n'
+    try:
+        await raju.edit_text(out)
+    except MessageTooLong:
+        with open('users.txt', 'w+') as outfile:
+            outfile.write(out)
+        await message.reply_document('users.txt', caption="List Of Users")
